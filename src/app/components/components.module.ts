991d1a02576b12c0194df14f5e0cac17616e094b
INDRA\obeses <oscar.beses@gmail.com>
2021-02-03 11:02:19 +0100

    separo por componentes

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -14,6 +14,8 @@ import { ComponentsComponent } from './components.component';
 import { NotificationComponent } from './notification/notification.component';
 import { NgbdModalComponent } from './modal/modal.component';
 import { NgbdModalContent } from './modal/modal.component';
+import { ContactoComponent } from 'app/contacto/contacto.component';
+import { CarouselComponent } from './../carousel/carousel.component';
 
 @NgModule({
     imports: [
@@ -32,7 +34,9 @@ import { NgbdModalContent } from './modal/modal.component';
         NucleoiconsComponent,
         NotificationComponent,
         NgbdModalComponent,
-        NgbdModalContent
+        NgbdModalContent,
+        ContactoComponent,
+        CarouselComponent
     ],
     entryComponents: [NgbdModalContent],
     exports:[ ComponentsComponent ]
